391a016098f8f3226b0533cfafeea0be597d40fd
Christoforos Psychas <37118969+ChristoforosPsychas@users.noreply.github.com>
2026-03-08 19:53:07 +0200

    Create playRound function to determine the winner of the round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,14 +9,45 @@ function getComputerChoice() {
   else return "scissors";
 }
 
-const result = getComputerChoice();
-console.log(result);
-
 function getHumanChoice() {
   const choice = prompt("Choose rock, paper, or scissors");
 
   return choice;
 }
 
-const choice = getHumanChoice();
-console.log(choice);
+function playRound(humanChoice, computerChoice) {
+  const humanCh = humanChoice.toLowerCase();
+
+  if (humanCh === "rock" && computerChoice === "paper") {
+    computerScore++;
+    return console.log("Computer wins! Paper beats Rock!");
+  } else if (humanCh === "scissors" && computerChoice === "paper") {
+    humanScore++;
+    return console.log("You win! Scissors beats Paper!");
+  } else if (humanCh === "paper" && computerChoice === "rock") {
+    humanScore++;
+    return console.log("You win! Paper beats Rock!");
+  } else if (humanCh === "scissors" && computerChoice === "rock") {
+    computerScore++;
+    return console.log("Computer wins! Rock beats Scissors!");
+  } else if (humanCh === "paper" && computerChoice === "scissors") {
+    computerScore++;
+    return console.log("Computer wins! Scissors beats Paper!");
+  } else if (humanCh === "rock" && computerChoice === "scissors") {
+    humanScore++;
+    return console.log("You win! Rock beats Scissors!");
+  } else {
+    return console.log("It's a tie!");
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log(`You chose: ${humanSelection}`);
+console.log(`Computer chose: ${computerSelection}`);
+
+console.log(`Your score: ${humanScore}`);
+console.log(`Computer score: ${computerScore}`);

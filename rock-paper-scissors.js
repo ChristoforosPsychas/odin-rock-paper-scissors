60b79b17520126d000cfc3de389873125582901b
Christoforos Psychas <37118969+ChristoforosPsychas@users.noreply.github.com>
2026-03-08 19:24:33 +0200

    Create getComputerChoice function

---
+++ rock-paper-scissors.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+  const number = Math.floor(Math.random() * 3);
+
+  if (number === 0) return "rock";
+  else if (number === 1) return "paper";
+  else return "scissors";
+}
+
+const result = getComputerChoice();
+console.log(result);
